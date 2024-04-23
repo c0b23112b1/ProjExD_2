5820f4f81e32e08c3500bb60b581a1b6b3c7cde2
Nita <C0B23112b1@edu.teu.ac.jp>
2024-04-23 15:09:30 +0900

    枠内判定追加

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,20 @@ DELTA = { #移動量辞書
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(obj_rct:pg.Rect) -> tuple[bool,bool]:
+    """
+    こうかとんRect　または　爆弾Rectの画面内外判定の関数
+    引数：こうかとんRect，または，爆弾Rect
+    戻り値：横方向判定結果，縦方向判定結果
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right: #画像の右端，左端
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko,tate
+
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -22,8 +36,7 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400 #初期配置
-    clock = pg.time.Clock()
-    tmr = 0
+    #爆弾
     bd_img = pg.Surface((20,20)) #1辺の長さ
     bd_img.set_colorkey((0,0,0)) #色変え
     pg.draw.circle(bd_img,(255,0,0),(10,10),10) #引数，色，中心座標，半径
@@ -31,6 +44,9 @@ def main():
     bd_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
     vx,vy = +5,+5
 
+
+    clock = pg.time.Clock()
+    tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -45,11 +61,23 @@ def main():
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
 
         screen.blit(kk_img, kk_rct)
 
+        bd_rct.move_ip(vx,vy) #爆弾の移動
+        screen.blit(bd_img,bd_rct)
+
+
+        #爆弾
         bd_rct.move_ip(vx,vy)
         screen.blit(bd_img,bd_rct)
+        yoko,tate = check_bound(bd_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
 
         pg.display.update()
         tmr += 1
